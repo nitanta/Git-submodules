2df86cde4717e7639510ffce6bd0224d5e71d629
Nitanta Adhikari <nitanta.adh007@gmail.com>
2023-04-11 06:24:17 +0545

    Initial Commit

---
+++ Git_submodules/Git_submodulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_submodulesApp.swift
+//  Git_submodules
+//
+//  Created by Nitanta Adhikari on 11/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Git_submodulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
